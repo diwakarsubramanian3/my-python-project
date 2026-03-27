736bf53fb7980364710f09c10266f29c6e9196c7
diwakarsubramanian3 <diwakarsubramanian.3@gmail.com>
2026-03-27 22:28:35 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,22 @@
-def greet(name):
-    return f"Hello, {name}!"
+todos = []
+while True:
+    user_action = input("Type add, show, edit or exit :")
 
-if __name__ == "__main__":
-    print(greet("Diwakar"))
+    match user_action:
+        case "add" :
+            todo = input("Enter a todo:")
+            todos.append(todo)
+
+        case "show"  :
+            for item in todos:
+                print(item)
+        case "edit":
+            number = int(input("Number of the todo to edit:"))
+            number = number-1
+            new_todo = input("Enter a new todo:")
+            todos[number] = new_todo
+
+        case "exit":
+            break
+
+print("Bye!")
