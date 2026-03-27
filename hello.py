a3be7043c3112c63fb847262cd250f93a522ad5b
diwakarsubramanian3 <diwakarsubramanian.3@gmail.com>
2026-03-27 22:06:16 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,22 +1,5 @@
-todos = []
-while True:
-    user_action = input("Type add, show, edit or exit :")
+def greet(name):
+    return f"Hello, {name}!"
 
-    match user_action:
-        case "add" :
-            todo = input("Enter a todo:")
-            todos.append(todo)
-
-        case "show"  :
-            for item in todos:
-                print(item)
-        case "edit":
-            number = int(input("Number of the todo to edit:"))
-            number = number-1
-            new_todo = input("Enter a new todo:")
-            todos[number] = new_todo
-
-        case "exit":
-            break
-
-print("Bye!")
+if __name__ == "__main__":
+    print(greet("Diwakar"))
