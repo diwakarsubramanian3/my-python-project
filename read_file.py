f093516757f70fb0a5fc606d6fe37b59a2d4ebf8
diwakarsubramanian3 <diwakarsubramanian.3@gmail.com>
2026-04-01 14:09:43 +0530

    Create read_file.py

---
+++ read_file.py
@@ -0,0 +1,40 @@
+
+while True:
+    user_action = input("Type add, show, edit or exit :")
+    user_action = user_action.strip()
+
+    match user_action:
+        case "add" :
+            todo = input("Enter a todo:") + "\n"               # +\n -this is to enter a break line and this will give a space or line between items
+
+            file = open('todos.txt', 'r')                      # this will open todos.txt file and 'r' represent read operation
+            todos = file.readlines()                           # this will read the files in txt file and will store it in list
+            file.close()                                       #this will close the txt file
+
+            todos.append(todo)
+
+            file = open('todos.txt', 'w')                      #we use open() object for opening the txt files and W is for write operation and it also overwrites the file
+            file.writelines(todos)                             # this is will write a input of todos to text file
+            file.close()
+
+        case "show"  :
+            for index,item in enumerate(todos):
+                row = f"{index}-{item}"
+                print(row)
+
+        case "edit":
+            number = int(input("Number of the todo to edit:"))
+            number = number-1
+            new_todo = input("Enter a new todo:")
+            todos[number] = new_todo
+
+        case "complete" :
+            number = int(input("Number of the todo to complete:"))
+            todos.pop(number -1)
+
+        case "exit":
+            break
+
+print("Bye!")
+
+
